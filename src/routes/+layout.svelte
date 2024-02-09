2ea10b4c186189fd0de57b9d90d1b5cd24b587f2
Nipodemos <nipodemos13@gmail.com>
2024-02-08 23:10:29 -0300

    inicializa a store no arquivo de layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { createDivisaoStore } from '$lib/divisaoStore.svelte';
+
+	interface Props {
+		data: {
+			pessoas: IPessoa[];
+			despesas: IDespesa[];
+		};
+		children: any;
+	}
+	let { children, data } = $props<Props>();
+	$inspect(data.pessoas);
+	createDivisaoStore(data.pessoas, data.despesas);
+</script>
+
+{@render children()}
