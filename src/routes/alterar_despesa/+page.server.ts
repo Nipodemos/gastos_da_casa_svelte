9c6652cc4ce183c31b4d8cc2b96eb7baa7c8d60b
Nipodemos <nipodemos13@gmail.com>
2024-02-12 21:32:22 -0300

    usando superforms pra controlar o form

--- src/routes/alterar_despesa/+page.server.ts
+++ src/routes/alterar_despesa/+page.server.ts
@@ -1,6 +1,51 @@
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { z } from 'zod';
 import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+import { getDivisaoStore } from '$lib/divisaoStore.svelte';
 
-export const load: PageServerLoad = (async ({ url }) => {
-	const id = url.searchParams.get('id');
-	return { id };
-}) satisfies PageServerLoad;
+const schema = z.object({
+	id: z.number(),
+	descricao: z.string().min(2, 'Mínimo de 2 caracteres').max(100, 'Máximo de 100 caracteres '),
+	valor: z.number().min(0.01, 'Valor não pode ser zero').nonnegative('Valor não pode ser negativo')
+});
+
+export const load: PageServerLoad = async ({ url }) => {
+	const id = Number(url.searchParams.get('id'));
+	const descricao = url.searchParams.get('descricao');
+	const valor = Number(url.searchParams.get('valor'));
+	if (id && descricao && valor) {
+		const form = await superValidate({ id, descricao, valor }, zod(schema));
+		return { form };
+	}
+	const form = await superValidate(zod(schema));
+
+	return { form };
+};
+
+export const actions = {
+	default: async ({ request }) => {
+		const form = await superValidate(request, zod(schema));
+		console.log(form);
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		const novaDespesa: IDespesa = {
+			id: form.data.id,
+			descricao: form.data.descricao,
+			valor: form.data.valor
+		};
+
+		const divisaoStore = getDivisaoStore();
+		const result = divisaoStore.alterarDespesa(novaDespesa);
+
+		if (!result) {
+			return fail(500, { form });
+		}
+
+		return { form };
+	}
+};
--- src/routes/alterar_despesa/+page.svelte
+++ src/routes/alterar_despesa/+page.svelte
@@ -1,40 +1,54 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { getDivisaoStore } from '$lib/divisaoStore.svelte';
-	let title = 'Alterar Despesa';
-	const url = $page.url;
+	import { superForm } from 'sveltekit-superforms';
+	import SuperDebug from 'sveltekit-superforms';
 
-	console.log(url);
+	const { data } = $props();
+
+	const { form, errors, constraints, enhance } = superForm(data.form);
+
+	const url = $page.url;
 	let idParam = url.searchParams.get('id');
-	if (!idParam) {
-		title = 'Adicionar Despesa';
-		idParam = '0';
-	}
-	let id = parseInt(idParam);
-	let descricao = '';
-	let valor = 0;
-	const divisaoStore = getDivisaoStore();
-	let despesa = divisaoStore.despesas!.find((d) => d.id === id);
-	if (despesa) {
-		descricao = despesa.descricao;
-		valor = despesa.valor;
-	}
+	let title = $state(idParam ? 'Alterar Despesa' : 'Adicionar Despesa');
 </script>
 
 <h1 class="text-center">{title}</h1>
+<SuperDebug data={$form} />
+<form
+	action=""
+	method="POST"
+	class="container d-flex flex-column justify-content-center align-items-center"
+	use:enhance
+>
+	<div class="mb-3">
+		<input type="hidden" name="id" bind:value={$form.id} />
 
-<div class="mb-3 d-flex justify-content-center">
-	<input type="hidden" name="id" bind:value={id} />
+		<label for="descricao">Descrição:</label>
+		<input
+			type="text"
+			id="descricao"
+			name="descricao"
+			bind:value={$form.descricao}
+			{...$constraints.descricao}
+			class="form-control"
+		/>
+		<div class="invalid-feedback">{$errors?.descricao}</div>
+	</div>
 
-	<label for="descricao">Descrição:</label>
-	<input type="text" id="descricao" bind:value={descricao} class="form-control" />
-</div>
+	<div class="mb-3">
+		<label for="valor">Valor:</label>
+		<input
+			type="number"
+			id="valor"
+			name="valor"
+			bind:value={$form.valor}
+			{...$constraints.valor}
+			class="form-control"
+		/>
+		<div class="invalid-feedback">{$errors?.valor}</div>
+	</div>
 
-<div class="mb-3 d-flex justify-content-center">
-	<label for="valor">Valor:</label>
-	<input type="number" id="valor" bind:value={valor} class="form-control" />
-</div>
-
-<div class="d-flex justify-content-center">
-	<button class="btn btn-primary">Salvar</button>
-</div>
+	<div class="">
+		<button class="btn btn-primary">Salvar</button>
+	</div>
+</form>
