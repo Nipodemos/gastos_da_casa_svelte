3cafa98ddd67d6292e7483ee5d47efddf54249cf
Nipodemos <nipodemos13@gmail.com>
2024-02-12 21:31:28 -0300

    usando a classe de database pra pegar os dados da nuvem

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,20 +1,7 @@
+import { JsonDatabase } from '$lib/database';
 import type { LayoutServerLoad } from './$types';
-import { env } from '$env/dynamic/private';
 export const load: LayoutServerLoad = async () => {
-	const apiPassword = env.API_PASSWORD;
-	const apiBinKey = env.API_BIN_KEY;
-	if (!apiPassword || !apiBinKey) {
-		throw new Error('API_PASSWORD ou API_BIN_KEY não definidos');
-	}
-	// site pra eu acessar o bin: https://extendsclass.com/json-storage.html
-	const res = await fetch('https://json.extendsclass.com/bin/' + apiBinKey, {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-			'Security-key': apiPassword
-		}
-	});
-	const jsonBin: jsonBinType = await res.json();
-
+	const database = new JsonDatabase();
+	const jsonBin = await database.getAll();
 	return jsonBin;
 };
